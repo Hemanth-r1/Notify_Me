3402eaba4536cc407b8a861fbbabd48e6df01335
hemanth-r1 <hemanth.r8892@gmail.com>
2021-04-06 13:55:37 +0530

    intial commit

---
+++ app/src/main/java/in/hr/notifyme/MainActivity.java
@@ -0,0 +1,69 @@
+package in.hr.notifyme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+    private NotificationManager notificationManager;
+    private static final int NOTIFICATION_ID = 0;
+
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID).setContentTitle("YOu have been NOTIFIED!")
+                .setContentText("This is your notification from main activity ln 26").setSmallIcon(R.drawable.ic_android);
+        return notificationBuilder;
+    }
+
+    public void createNotificationChannel(){
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            // Create a Notification Channel
+
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID, "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+    }
+
+    public void sendNotification(){
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+
+        createNotificationChannel();
+    }
+
+}
